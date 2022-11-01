433c8dbb7cf275631c04c31937d5fb2d007ad455
Thibaut Bournonville <tbournon@k0r4p11.42mulhouse.fr>
2022-11-01 12:36:29 +0100

    FINISHED memset

--- Libft/memset.c
+++ Libft/memset.c
@@ -6,12 +6,11 @@
 /*   By: tbournon <tbournon@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/01 12:10:47 by tbournon          #+#    #+#             */
-/*   Updated: 2022/11/01 12:16:52 by tbournon         ###   ########.fr       */
+/*   Updated: 2022/11/01 12:31:53 by tbournon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
-// #include <stdio.h>
 
 void	*ft_memset(void *b, int c, size_t len)
 {
@@ -28,14 +27,3 @@ void	*ft_memset(void *b, int c, size_t len)
 	}
 	return (b);
 }
-
-/* int main()
-{
-	char *array = "bonjour";
-	printf("%s", array);
-
-	ft_memset(array, 'a', 3);
-	printf("%s", array);
-
-	return 0;
-} */
