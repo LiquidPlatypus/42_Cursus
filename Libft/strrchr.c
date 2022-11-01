9f775731e071c31877b9423ced4517d754a2ebc4
Thibaut Bournonville <tbournon@k0r4p11.42mulhouse.fr>
2022-11-01 16:16:07 +0100

    ADD strrchr

--- Libft/strrchr.c
+++ Libft/strrchr.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   strchr.c                                           :+:      :+:    :+:   */
+/*   strrchr.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: tbournon <tbournon@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/11/01 15:33:36 by tbournon          #+#    #+#             */
-/*   Updated: 2022/11/01 16:15:13 by tbournon         ###   ########.fr       */
+/*   Created: 2022/11/01 16:05:13 by tbournon          #+#    #+#             */
+/*   Updated: 2022/11/01 16:15:22 by tbournon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,13 @@
 
 char	*ft_strchr(const char *s, int c)
 {
-	int			index;
-	
+	int	index;
+
 	index = 0;
-	while (s[index] != c && s[index] != '\0')
+	while (s[index] != '\0')
 		index++;
+	while (s[index] != c || index == 0)
+		index--;
 	if (s[index] == c)
 		return ((char *)&s[index]);
 	else
