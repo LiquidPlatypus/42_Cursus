e19882156df5e96b38ba9c630b9daa2a06220806
Thibaut Bournonville <tbournon@k0r4p11.42mulhouse.fr>
2022-11-01 13:08:00 +0100

    ADD memmove

--- Libft/memmove.c
+++ Libft/memmove.c
@@ -6,12 +6,11 @@
 /*   By: tbournon <tbournon@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/01 12:53:07 by tbournon          #+#    #+#             */
-/*   Updated: 2022/11/01 13:06:44 by tbournon         ###   ########.fr       */
+/*   Updated: 2022/11/01 13:07:47 by tbournon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
-#include <stdio.h>
 
 void	*ft_memmove(void *dst, const void *src, size_t len)
 {
@@ -33,17 +32,4 @@ void	*ft_memmove(void *dst, const void *src, size_t len)
 	dst = p;
 	src = q;
 	return (dst);
-}
-
-int main()
-{
-	char dst[] = "bonjour";
-	char src[] = "test";
-
-	printf("%s\t%s\n", dst, src);
-
-	ft_memmove(dst, src, 3);
-	printf("%s", dst);
-
-	return 0;
 }
