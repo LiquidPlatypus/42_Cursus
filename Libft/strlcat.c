3b860574b5c3e986e459941e81078192ffe6c704
Thibaut Bournonville <tbournon@k0r4p11.42mulhouse.fr>
2022-11-01 15:20:30 +0100

    MODIFIED strlcat

--- Libft/strlcat.c
+++ Libft/strlcat.c
@@ -6,12 +6,11 @@
 /*   By: tbournon <tbournon@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/01 13:59:39 by tbournon          #+#    #+#             */
-/*   Updated: 2022/11/01 15:17:04 by tbournon         ###   ########.fr       */
+/*   Updated: 2022/11/01 15:20:14 by tbournon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
-#include <stdio.h>
 
 int	ft_strlcat(char *restrict dst, const char *restrict src, size_t dstsize)
 {
@@ -40,17 +39,4 @@ int	ft_strlcat(char *restrict dst, const char *restrict src, size_t dstsize)
 	if (dstsize == 1 || *src == 0)
 		*dst = '\0';
 	return (src_length + x);
-}
-
-int main()
-{
-	char src[] = "bonjour";
-	char dest[] = "test";
-
-	printf("%s\t%s\n", src, dest);
-
-	ft_strlcat(dest, src, 0);
-	printf("%s", dest);
-	
-	return 0;
 }
