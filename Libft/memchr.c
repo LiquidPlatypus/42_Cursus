7d9dd0d42a6045b9a0fd7f82cfd00fdfaa79a79a
Thibaut Bournonville <tbournon@k0r4p11.42mulhouse.fr>
2022-11-02 09:46:15 +0100

    ADDED memchr

--- Libft/memchr.c
+++ Libft/memchr.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   memcmp.c                                           :+:      :+:    :+:   */
+/*   memchr.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: tbournon <tbournon@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/01 16:29:59 by tbournon          #+#    #+#             */
-/*   Updated: 2022/11/01 16:30:40 by tbournon         ###   ########.fr       */
+/*   Updated: 2022/11/02 09:45:49 by tbournon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,15 @@
 
 void	*ft_memchr(const void *s, int c, size_t n)
 {
-	
+	unsigned int		index;
+	const unsigned char	*p;
+
+	index = 0;
+	p = s;
+	while (p[index] != c && p[index] != '\0' && n > 0)
+		index++;
+	if (p[index] == c)
+		return ((void *)&p[index]);
+	else
+		return (NULL);
 }
