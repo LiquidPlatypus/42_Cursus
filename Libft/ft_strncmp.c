875fa3a14d655217a28846f8f1d9859ba6d8c666
Thibaut Bournonville <tbournon@k0r4p11.42mulhouse.fr>
2022-11-03 11:37:52 +0100

    RECTIFIED strncmp

--- Libft/ft_strncmp.c
+++ Libft/ft_strncmp.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   strncmp.c                                          :+:      :+:    :+:   */
+/*   ft_strncmp.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: tbournon <tbournon@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/01 16:17:47 by tbournon          #+#    #+#             */
-/*   Updated: 2022/11/01 16:25:37 by tbournon         ###   ########.fr       */
+/*   Updated: 2022/11/03 11:37:05 by tbournon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,14 +15,18 @@
 int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
 	int	x;
+	unsigned char *p;
+	unsigned char *q;
 
 	x = 0;
-	while (s1[x] && s1[x] == s2[x] && n > 0)
+	p = (unsigned char *)s1;
+	q = (unsigned char *)s2;
+	while (p[x] && p[x] == q[x] && n > 0)
 	{
 		x++;
 		n--;
 	}
 	if (n == 0)
 		return (0);
-	return (s1[x] - s2[x]);
+	return (p[x] - q[x]);
 }
