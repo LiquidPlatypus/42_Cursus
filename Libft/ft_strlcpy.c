dbd8afc0cdcad98ebde738ab54b02f7f3fe5179c
Thibaut Bournonville <tbournon@k0r4p11.42mulhouse.fr>
2022-11-03 13:00:10 +0100

    MODIFIED some files

--- Libft/ft_strlcpy.c
+++ Libft/ft_strlcpy.c
@@ -6,16 +6,18 @@
 /*   By: tbournon <tbournon@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/01 13:09:01 by tbournon          #+#    #+#             */
-/*   Updated: 2022/11/03 10:46:31 by tbournon         ###   ########.fr       */
+/*   Updated: 2022/11/03 11:48:41 by tbournon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
 
-int	ft_strlcpy(char *dst, const char *src, size_t dstsize)
+size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize)
 {
-	unsigned int	x;
-	unsigned int	size_src;
+	size_t	x;
+	size_t	size_src;
 
 	x = 0;
 	size_src = 0;
@@ -24,6 +26,8 @@ int	ft_strlcpy(char *dst, const char *src, size_t dstsize)
 		size_src++;
 		x++;
 	}
+	if (size_src > dstsize)
+		return (0);
 	x = 0;
 	while (src[x] != '\0' && x < dstsize)
 	{
@@ -33,4 +37,16 @@ int	ft_strlcpy(char *dst, const char *src, size_t dstsize)
 	if (dstsize < size_src)
 		dst[dstsize - 1] = '\0';
 	return (size_src);
+}
+
+int main()
+{
+	char src[] = "bonjour";
+	char dst[] = "test";
+	int dstsize = 3;
+
+	printf("%lu\n", ft_strlcpy(src, dst, dstsize));
+	printf("%lu", strlcpy(src, dst, dstsize));
+
+	return 0;
 }
