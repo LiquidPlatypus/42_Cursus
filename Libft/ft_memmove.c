7ec711ef808bf9c9db7d553dbbffaa5c2ddb339a
Thibaut Bournonville <tbournon@k0r4p11.42mulhouse.fr>
2022-11-04 15:37:26 +0100

    RECTIFIED libft

--- Libft/ft_memmove.c
+++ Libft/ft_memmove.c
@@ -6,11 +6,13 @@
 /*   By: tbournon <tbournon@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/01 12:53:07 by tbournon          #+#    #+#             */
-/*   Updated: 2022/11/03 16:33:14 by tbournon         ###   ########.fr       */
+/*   Updated: 2022/11/04 15:14:08 by tbournon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
 
 void	*ft_memmove(void *dst, const void *src, size_t len)
 {
@@ -33,3 +35,14 @@ void	*ft_memmove(void *dst, const void *src, size_t len)
 	src = q;
 	return (dst);
 }
+
+/*int main()
+{
+	char src[] = "lorem ipum dolor sit a";
+	char dst[40];
+
+	printf("%s\n", ft_memmove(dst, src, 8));
+	printf("%s", memmove(dst, src, 8));
+
+	return 0;
+} */
--- Libft/libft.h
+++ Libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: tbournon <tbournon@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/02 14:07:30 by tbournon          #+#    #+#             */
-/*   Updated: 2022/11/02 15:15:25 by tbournon         ###   ########.fr       */
+/*   Updated: 2022/11/04 15:36:12 by tbournon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,7 +33,7 @@ int		ft_strlcpy(char *dst, const char *src, size_t dstsize);
 int		ft_strlen(char *str);
 int		ft_strncmp(const char *s1, const char *s2, size_t n);
 char	*ft_strnstr(const char *haystack, const char *needle, size_t len);
-char	*ft_strchr(const char *s, int c);
+char	*ft_strrchr(const char *s, int c);
 int		ft_tolower(int c);
 int		ft_toupper(int c);
 
