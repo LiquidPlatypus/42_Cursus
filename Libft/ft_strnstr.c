47b6cc4a48cffa88224c83fbe23904a2c41021cc
Thibaut Bournonville <tbournon@k0r4p11.42mulhouse.fr>
2022-11-05 11:06:13 +0100

    FINISHED strnstr

--- Libft/ft_strnstr.c
+++ Libft/ft_strnstr.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   strnstr.c                                          :+:      :+:    :+:   */
+/*   ft_strnstr.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: tbournon <tbournon@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/02 09:57:42 by tbournon          #+#    #+#             */
-/*   Updated: 2022/11/02 13:47:52 by tbournon         ###   ########.fr       */
+/*   Updated: 2022/11/05 11:05:35 by tbournon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,25 +14,23 @@
 
 char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 {
-	int	index1;
-	int	index2;
+	size_t	index1;
+	size_t	index2;
 
 	index1 = 0;
 	index2 = 0;
 	if (needle[index2] == '\0')
 		return ((char *)haystack);
-	while (haystack[index1] != '\0')
+	while (haystack[index1] && index1 < len)
 	{
-		while ((haystack[index1 + index2] == needle[index2])
-			&& haystack[index1 + index2] != '\0' && len > 0)
-		{
+		index2 = 0;
+		while (haystack[index1 + index2] && needle[index2]
+			&& index1 + index2 < len
+			&& haystack[index1 + index2] == needle[index2])
 			index2++;
-			len--;
-		}
 		if (needle[index2] == '\0')
 			return ((char *)haystack + index1);
 		index1++;
-		index2 = 0;
 	}
 	return (NULL);
 }
