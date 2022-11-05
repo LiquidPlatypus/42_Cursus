ca15155c050984612ad707c885011e9db187bf76
Thibaut Bournonville <tbournon@k0r4p11.42mulhouse.fr>
2022-11-05 13:12:50 +0100

    UNFINISHED strlcat

--- Libft/ft_strlcat.c
+++ Libft/ft_strlcat.c
@@ -6,11 +6,13 @@
 /*   By: tbournon <tbournon@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/01 13:59:39 by tbournon          #+#    #+#             */
-/*   Updated: 2022/11/02 15:09:47 by tbournon         ###   ########.fr       */
+/*   Updated: 2022/11/05 12:08:50 by tbournon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
+// #include <stdio.h>
+// #include <string.h>
 
 int	ft_strlcat(char *dst, const char *src, size_t dstsize)
 {
@@ -21,7 +23,7 @@ int	ft_strlcat(char *dst, const char *src, size_t dstsize)
 	x = 0;
 	y = 0;
 	src_length = 0;
-	while (src[x] != '\0')
+	while (src[x])
 	{
 		src_length++;
 		x++;
@@ -39,3 +41,16 @@ int	ft_strlcat(char *dst, const char *src, size_t dstsize)
 		*dst = '\0';
 	return (src_length + x);
 }
+
+/* int main()
+{
+	char str[] = "the cake is a lie !\0I'm hidden lol\r\n";
+	// char buff1[0xF00] = "there is no stars in the sky";
+	char buff2[0xF00] = "there is no stars in the sky";
+	size_t max = strlen("the cake is a lie !\0I'm hidden lol\r\n") + 4;
+
+	printf("%d\n", ft_strlcat(buff2, str, max));
+	printf("%lu", strlcat(buff2, str, max));
+
+	return 0;
+} */
