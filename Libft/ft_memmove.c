ed372d8e0c81d61e2e1b89cdf7140fa8996a3e17
Thibaut Bournonville <tbournon@k0r4p11.42mulhouse.fr>
2022-11-05 13:12:29 +0100

    FINISHED memmove

--- Libft/ft_memmove.c
+++ Libft/ft_memmove.c
@@ -6,43 +6,35 @@
 /*   By: tbournon <tbournon@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/01 12:53:07 by tbournon          #+#    #+#             */
-/*   Updated: 2022/11/04 15:14:08 by tbournon         ###   ########.fr       */
+/*   Updated: 2022/11/05 13:11:49 by tbournon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
-#include <stdio.h>
-#include <string.h>
 
 void	*ft_memmove(void *dst, const void *src, size_t len)
 {
 	unsigned int		x;
 	unsigned char		*p;
 	const unsigned char	*q;
-	const unsigned char	*buffer;
 
 	x = 0;
 	p = dst;
 	q = src;
-	buffer = q;
-	while (len > 0)
+	if ((p == NULL && q == NULL) && len >= 0)
+		return (p);
+	if (p > q)
 	{
-		p[x] = buffer[x];
-		x++;
-		len--;
+		while (len-- > 0)
+			p[len] = q[len];
 	}
-	dst = p;
-	src = q;
-	return (dst);
+	else
+	{
+		while (len-- > 0)
+		{
+			p[x] = q[x];
+			x++;
+		}
+	}
+	return ((void *) p);
 }
-
-/*int main()
-{
-	char src[] = "lorem ipum dolor sit a";
-	char dst[40];
-
-	printf("%s\n", ft_memmove(dst, src, 8));
-	printf("%s", memmove(dst, src, 8));
-
-	return 0;
-} */
