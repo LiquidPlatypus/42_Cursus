8670e53b373a5d22616563885889ca901caebaf8
Thibaut Bournonville <tbournon@k0r4p11.42mulhouse.fr>
2022-11-07 16:40:11 +0100

    RECTIFIED libft

--- Libft/libft.h
+++ Libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: tbournon <tbournon@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/02 14:07:30 by tbournon          #+#    #+#             */
-/*   Updated: 2022/11/04 15:36:12 by tbournon         ###   ########.fr       */
+/*   Updated: 2022/11/07 16:39:17 by tbournon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,7 +30,7 @@ void	*ft_memset(void *b, int c, size_t len);
 char	*ft_strchr(const char *s, int c);
 int		ft_strlcat(char *dst, const char *src, size_t dstsize);
 int		ft_strlcpy(char *dst, const char *src, size_t dstsize);
-int		ft_strlen(char *str);
+int		ft_strlen(const char *str);
 int		ft_strncmp(const char *s1, const char *s2, size_t n);
 char	*ft_strnstr(const char *haystack, const char *needle, size_t len);
 char	*ft_strrchr(const char *s, int c);
