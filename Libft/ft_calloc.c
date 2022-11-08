fc02a61443de97da7e65a57cf7b6bb058878416b
Thibaut Bournonville <tbournon@k0r4p11.42mulhouse.fr>
2022-11-08 16:17:59 +0100

    UNFINISHED calloc

---
+++ Libft/ft_calloc.c
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_calloc.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tbournon <tbournon@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/08 15:53:08 by tbournon          #+#    #+#             */
+/*   Updated: 2022/11/08 16:07:43 by tbournon         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_calloc(size_t count, size_t size)
+{
+	void	*mem;
+
+	mem = (size_t *)malloc(count * sizeof(size));
+	if (mem == NULL)
+		return (NULL);
+	return (mem);
+}
