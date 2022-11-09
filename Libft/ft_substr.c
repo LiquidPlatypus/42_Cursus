6fd0c28f02f9d6b7caeb15d6bd43ac3f93e511d9
Liquid Platypus <104024934+LiquidPlatypus@users.noreply.github.com>
2022-11-09 12:09:34 +0100

    FINISHED substr

--- Libft/ft_substr.c
+++ Libft/ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: tbournon <tbournon@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/09 10:04:25 by tbournon          #+#    #+#             */
-/*   Updated: 2022/11/09 10:05:06 by tbournon         ###   ########.fr       */
+/*   Updated: 2022/11/09 12:09:21 by tbournon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,18 @@
 
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
-	
+	size_t	index;
+	char	*s2;
+
+	index = 0;
+	s2 = (char *)malloc(sizeof(char) * (len + 1));
+	if (s2 == NULL)
+		return (NULL);
+	if ((int)start < ft_strlen(s))
+	{
+		while (index < len)
+		s2[index++] = s[start++];
+	}
+	s2[index] = '\0';
+	return (s2);
 }
