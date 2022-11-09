7ccacd278720c4749ac3b8371ed3d245cf1599a8
Thibaut Bournonville <tbournon@k0r4p11.42mulhouse.fr>
2022-11-09 10:03:22 +0100

    FINISHED strdup

--- Libft/ft_strdup.c
+++ Libft/ft_strdup.c
@@ -6,22 +6,23 @@
 /*   By: tbournon <tbournon@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/09 09:35:02 by tbournon          #+#    #+#             */
-/*   Updated: 2022/11/09 09:43:07 by tbournon         ###   ########.fr       */
+/*   Updated: 2022/11/09 10:02:53 by tbournon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <stdio.h>
+#include <string.h>
 
-char	*strdup(const char *s1)
+char	*ft_strdup(const char *s1)
 {
 	int		x;
 	char	*s2;
 
-	x = 0;
-	s2 = (char *)malloc(sizeof(s1));
+	x = ft_strlen(s1) + 1;
+	s2 = malloc(x);
 	if (s2 == NULL)
 		return (NULL);
-	while (s1[x++])
-		s2[x] = s1[x];
-	return (*s2);
+	ft_memcpy(s2, s1, x);
+	return (s2);
 }
