04ff66634b5e7064c659b83e1a46d43f14e7b246
Liquid Platypus <104024934+LiquidPlatypus@users.noreply.github.com>
2022-11-10 17:19:06 +0100

    Update ft_putstr_fd.c

--- Libft/ft_putstr_fd.c
+++ Libft/ft_putstr_fd.c
@@ -6,7 +6,7 @@
 /*   By: tbournon <tbournon@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/10 17:16:02 by tbournon          #+#    #+#             */
-/*   Updated: 2022/11/10 17:16:34 by tbournon         ###   ########.fr       */
+/*   Updated: 2022/11/10 17:18:25 by tbournon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,5 +15,5 @@
 
 void	ft_putstr_fd(char *s, int fd)
 {
-
+	write(fd, s, ft_strlen(s));
 }
