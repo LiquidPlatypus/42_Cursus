44c04f4b56d43e385c8819c88b3ece52b2c08e21
Liquid Platypus <104024934+LiquidPlatypus@users.noreply.github.com>
2022-11-11 12:56:09 +0100

    Update ft_putnbr_fd.c

--- Libft/ft_putnbr_fd.c
+++ Libft/ft_putnbr_fd.c
@@ -6,7 +6,7 @@
 /*   By: tbournon <tbournon@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/11 09:35:32 by tbournon          #+#    #+#             */
-/*   Updated: 2022/11/11 09:49:29 by tbournon         ###   ########.fr       */
+/*   Updated: 2022/11/11 12:55:20 by tbournon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,9 +15,7 @@
 void	ft_putnbr_fd(int n, int fd)
 {
 	if (n == -2147483648)
-	{
 		ft_putstr_fd("-2147483648", fd);
-	}
 	else if (n < 0)
 	{
 		ft_putchar_fd('-', fd);
