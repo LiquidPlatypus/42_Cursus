b2cebc088ddf87c96d957ac7971ac9ee1bf18556
Liquid Platypus <104024934+LiquidPlatypus@users.noreply.github.com>
2022-11-11 11:14:39 +0100

    Update ft_itoa.c

--- Libft/ft_itoa.c
+++ Libft/ft_itoa.c
@@ -6,13 +6,53 @@
 /*   By: tbournon <tbournon@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/11 09:54:46 by tbournon          #+#    #+#             */
-/*   Updated: 2022/11/11 09:55:10 by tbournon         ###   ########.fr       */
+/*   Updated: 2022/11/11 11:14:21 by tbournon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <stdio.h>
+
+int	digit_count(int n)
+{
+	int	count;
+
+	count = 0;
+	while (n != 0)
+	{
+		n /= 10;
+		count++;
+	}
+	return (count);
+}
 
 char	*ft_itoa(int n)
 {
-	
+	int		dgt_count;
+	int		orig_n;
+	char	*final_str;
+
+	dgt_count = digit_count(n);
+	orig_n = n;
+	final_str = (char *)malloc(sizeof(char) * dgt_count + 1);
+	while (dgt_count--)
+	{
+		if (n == 0)
+			final_str[dgt_count] = '0';
+		else
+		{
+			final_str[dgt_count] = (n % 10) + 48;
+			n /= 10;
+		}
+	}
+	dgt_count = digit_count(orig_n);
+	final_str[dgt_count] = '\0';
+	return (final_str);
+}
+
+int main()
+{
+	printf("%s", ft_itoa(0));
+
+	return (0);
 }
