e5bb5a15c25903c7c61913d1fc8cfb15e14356f4
Liquid Platypus <104024934+LiquidPlatypus@users.noreply.github.com>
2022-11-14 15:36:07 +0100

    FINISHED strmapi

--- Libft/ft_strmapi.c
+++ Libft/ft_strmapi.c
@@ -6,11 +6,28 @@
 /*   By: tbournon <tbournon@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/14 11:24:07 by tbournon          #+#    #+#             */
-/*   Updated: 2022/11/14 11:24:45 by tbournon         ###   ########.fr       */
+/*   Updated: 2022/11/14 15:30:44 by tbournon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
+
 char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
 {
-	
+	int		index;
+	char	*mapi;
+
+	index = 0;
+	if (!s)
+		return (NULL);
+	mapi = (char *)malloc((sizeof(char)) * (ft_strlen(s) + 1));
+	if (!mapi)
+		return (NULL);
+	while (s[index])
+	{
+			mapi[index] = f(index, s[index]);
+			index++;
+	}
+	mapi[index] = '\0';
+	return (mapi);
 }
