9f2854aafedccecf10cd5f40df62f0f6a695556a
Liquid Platypus <104024934+LiquidPlatypus@users.noreply.github.com>
2022-11-14 16:23:19 +0100

    FINISHED striteri

--- Libft/ft_striteri.c
+++ Libft/ft_striteri.c
@@ -6,20 +6,23 @@
 /*   By: tbournon <tbournon@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/14 15:37:41 by tbournon          #+#    #+#             */
-/*   Updated: 2022/11/14 16:17:26 by tbournon         ###   ########.fr       */
+/*   Updated: 2022/11/14 16:22:17 by tbournon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include "libft.h"
 
 void	ft_striteri(char *s, void (*f)(unsigned int, char*))
 {
 	int	index;
 
 	index = 0;
-	if (!s)
-		return (NULL);
-	while (s[index])
+	if (s)
 	{
-		f(index, s[index]);
-		index++;
+		while (s[index])
+		{
+			f(index, &s[index]);
+			index++;
+		}
 	}
 }
