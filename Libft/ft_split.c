eeafcd3a20ecfbc2517651d66aa3744f4c236385
Liquid Platypus <104024934+LiquidPlatypus@users.noreply.github.com>
2022-11-15 12:49:54 +0100

    Update ft_split.c

--- Libft/ft_split.c
+++ Libft/ft_split.c
@@ -6,57 +6,75 @@
 /*   By: tbournon <tbournon@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/10 14:03:34 by tbournon          #+#    #+#             */
-/*   Updated: 2022/11/14 16:35:03 by tbournon         ###   ########.fr       */
+/*   Updated: 2022/11/15 12:49:39 by tbournon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <stdio.h>
 
 // ! À CORRIGER -----------------------------------------------------------
 
-int	substring_count(char *s, char c)
+int	substring_count(const char *s, char c)
 {
-	int	count;
+	size_t	index;
+	int		count;
+	int		words;
 
+	index = 0;
 	count = 0;
-	while (*s)
+	words = 1;
+	while (s[index])
 	{
-		if (*s == c)
+		if (words && s[index] != c)
+		{
 			count++;
-		s++;
+			words = 0;
+		}
+		if (s[index] == c)
+			words = 1;
+		index++;
 	}
 	return (count);
 }
 
+int	word_length(const char *s, char c, int *len)
+{
+	int	taille;
+
+	while (s[*len] == c)
+		*len++;
+	taille = *len;
+	while (s[*len])
+	{
+		if (s[*len] == c)
+			break ;
+		*len++;
+	}
+	return (ft_substr(s, taille, *len - taille));
+}
+
 char	**ft_split(char const *s, char c)
 {
-	int		x;
-	int		y;
-	int		z;
-	char	*str;
+	int		index;
+	int		len;
+	int		substrings;
 	char	**splited;
 
-	x = 0;
-	y = 0;
-	z = 0;
-	str = (char *)s;
+	index = 0;
+	substrings = substring_count(s, c);
 	if (!s)
 		return (NULL);
-	splited = (char **)malloc(sizeof(substring_count));
-	while (s[x] != '\0')
+	splited = (char **)malloc(sizeof(char *) * (substrings + 1));
+	if (splited == NULL)
+		return (NULL);
+	len = 0;
+	while (substrings--)
 	{
-		if (str[x] == c)
-		{
-			str[y] = '\0';
-			splited[z] = malloc(sizeof(str));
-			ft_strlcpy(splited[z], str, sizeof(str));
-			y = 0;
-			z++;
-			ft_memset((void *)str, 0, sizeof(s));
-		}
-		else
-			str[y++] = s[x];
-		x++;
+		splited[index] = word_length(s, c, &len);
+		index++;
 	}
+	splited[index] = NULL;
+	free((void *)splited);
 	return (splited);
 }
