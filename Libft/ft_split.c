b1717e1810379177a3282faa18c7beeb058b84a2
Liquid Platypus <104024934+LiquidPlatypus@users.noreply.github.com>
2022-11-16 11:26:54 +0100

    ENFIN FINI split

--- Libft/ft_split.c
+++ Libft/ft_split.c
@@ -6,88 +6,75 @@
 /*   By: tbournon <tbournon@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/10 14:03:34 by tbournon          #+#    #+#             */
-/*   Updated: 2022/11/15 15:32:55 by tbournon         ###   ########.fr       */
+/*   Updated: 2022/11/16 11:26:04 by tbournon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-#include <stdio.h>
 
-// ! À CORRIGER -----------------------------------------------------------
+void	**ft_free(void **array, size_t n)
+{
+	while (n)
+		free(array[--n]);
+	free(array);
+	return (0);
+}
 
-int	substring_count(const char *s, char c)
+size_t	ft_substrings_count(const char *s, char c)
+{
+	size_t	len;
+	size_t	index;
+
+	index = 0;
+	len = 0;
+	while (s[index++])
+	{
+		while (s[index] != c && s[index])
+			index++;
+		len++;
+		while (s[index] == c && s[index])
+			index++;
+	}
+	return (len);
+}
+
+char	**ft_spliteur(const char *s, char c, char **res)
+{
+	size_t	len;
+	size_t	index;
+	size_t	y;
+
+	index = 0;
+	y = 0;
+	while (s[y])
+	{
+		len = 0;
+		while (s[y] != c && s[y] && ++y)
+			len++;
+		res[index] = malloc(len + 1);
+		if (!res[index])
+			return ((char **)ft_free((void **) res, index));
+		ft_strlcpy(res[index++], &s[y - len], len + 1);
+		while (s[y] == c && s[y])
+			y++;
+	}
+	res[index] = 0;
+	return (res);
+}
+
+char	**ft_split(const char *s, char c)
 {
 	size_t	index;
-	int		count;
-	int		words;
-
-	index = 0;
-	count = 0;
-	words = 1;
-	while (s[index])
-	{
-		if (words && s[index] != c)
-		{
-			count++;
-			words = 0;
-		}
-		if (s[index] == c)
-			words = 1;
-		index++;
-	}
-	return (count);
-}
-
-char	*word_length(const char *s, char c, int *len)
-{
-	int	taille;
-
-	while (s[*len] == c)
-		*len += 1;
-	taille = *len;
-	while (s[*len])
-	{
-		if (s[*len] == c)
-			break ;
-		*len += 1;
-	}
-	return (ft_substr(s, taille, *len - taille));
-}
-
-void	ft_check_free(char **s, int substrings)
-{
-	int	index;
-
-	index = 0;
-	while (s[index])
-		index++;
-	if (index == substrings)
-		return ;
-	index = 0;
-	while (index <= substrings)
-		free(s[index++]);
-	// free(s);
-	s = NULL;
-}
-
-char	**ft_split(char const *s, char c)
-{
-	int		index;
-	int		len;
-	int		substrings;
 	char	**splited;
 
-	index = 0;
-	substrings = substring_count(s, c);
 	if (!s)
-		return (NULL);
-	splited = (char **)malloc(sizeof(char *) * (substrings + 1));
-	if (splited == NULL)
-		return (NULL);
-	len = 0;
-	while (index < substrings)
-		splited[index++] = word_length(s, c, &len);
-	splited[index] = NULL;
-	ft_check_free(splited, substrings);
-	return (splited);
+		return (0);
+	index = 0;
+	while (s[index] == c && s[index])
+		index++;
+	splited = (char **)malloc(sizeof(char *)
+			* (ft_substrings_count(&s[index], c) + 1));
+	if (!splited)
+		return (0);
+	return (ft_spliteur(&s[index], c, splited));
 }
