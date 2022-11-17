f45057b958d5ff76364e1f2db80dc1dcedeca63f
Liquid Platypus <104024934+LiquidPlatypus@users.noreply.github.com>
2022-11-17 09:58:13 +0100

    FINISHED lstlast

--- Libft/ft_lstlast.c
+++ Libft/ft_lstlast.c
@@ -6,7 +6,7 @@
 /*   By: tbournon <tbournon@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/17 09:37:19 by tbournon          #+#    #+#             */
-/*   Updated: 2022/11/17 09:38:06 by tbournon         ###   ########.fr       */
+/*   Updated: 2022/11/17 09:56:18 by tbournon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,10 @@
 
 t_list	*ft_lstlast(t_list *lst)
 {
-	
+	if (lst != NULL)
+	{
+		while (lst->next != NULL)
+			lst = lst->next;
+	}
+	return (lst);
 }
