448f9c90c082f8838cf4bf885e996d6848ff3add
Liquid Platypus <104024934+LiquidPlatypus@users.noreply.github.com>
2022-11-17 10:52:36 +0100

    ADDED lstdelone

--- Libft/libft.h
+++ Libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: tbournon <tbournon@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/02 14:07:30 by tbournon          #+#    #+#             */
-/*   Updated: 2022/11/17 10:00:55 by tbournon         ###   ########.fr       */
+/*   Updated: 2022/11/17 10:52:08 by tbournon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,6 +33,7 @@ int		ft_isprint(int c);
 char	*ft_itoa(int n);
 void	ft_lstadd_back(t_list **lst, t_list *new);
 void	ft_lstadd_front(t_list **lst, t_list *new);
+void	ft_lstdelone(t_list *lst, void (*del)(void *));
 t_list	*ft_lstlast(t_list *lst);
 t_list	*ft_lstnew(void	*content);
 int		ft_lstsize(t_list *lst);
