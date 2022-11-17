de9b615f60d922e08aca415046275f7d1eba467e
Liquid Platypus <104024934+LiquidPlatypus@users.noreply.github.com>
2022-11-17 12:18:19 +0100

    Update libft.h

--- Libft/libft.h
+++ Libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: tbournon <tbournon@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/02 14:07:30 by tbournon          #+#    #+#             */
-/*   Updated: 2022/11/17 11:59:01 by tbournon         ###   ########.fr       */
+/*   Updated: 2022/11/17 12:18:00 by tbournon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,7 +37,7 @@ void	ft_lstclear(t_list **lst, void (*del)(void *));
 void	ft_lstdelone(t_list *lst, void (*del)(void *));
 void	ft_lstiter(t_list *lst, void (*f)(void *));
 t_list	*ft_lstlast(t_list *lst);
-t_list	*ft_lstmap(t_list *lst, void *(*f)(void));
+t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void));
 t_list	*ft_lstnew(void	*content);
 int		ft_lstsize(t_list *lst);
 void	*ft_memchr(const void *s, int c, size_t n);
