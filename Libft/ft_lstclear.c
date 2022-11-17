34fbfb490d66cf1561c670580f5086d7af2f91ce
Liquid Platypus <104024934+LiquidPlatypus@users.noreply.github.com>
2022-11-17 11:39:03 +0100

    FINISHED lstclear

--- Libft/ft_lstclear.c
+++ Libft/ft_lstclear.c
@@ -6,7 +6,7 @@
 /*   By: tbournon <tbournon@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/17 11:06:05 by tbournon          #+#    #+#             */
-/*   Updated: 2022/11/17 11:21:11 by tbournon         ###   ########.fr       */
+/*   Updated: 2022/11/17 11:38:42 by tbournon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,16 @@
 
 void	ft_lstclear(t_list **lst, void (*del)(void *))
 {
-	
+	t_list	*tmp;
+
+	if (lst && del)
+	{
+		while (*lst != NULL)
+		{
+			tmp = (*lst)->next;
+			ft_lstdelone(*lst, del);
+			*lst = tmp;
+		}
+		*lst = NULL;
+	}
 }
