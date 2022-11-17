c2e9891030dc0d399dd3676ce2e47fa60f495b21
Liquid Platypus <104024934+LiquidPlatypus@users.noreply.github.com>
2022-11-17 10:48:48 +0100

    FINISHED lstadd_back

--- Libft/ft_lstadd_back.c
+++ Libft/ft_lstadd_back.c
@@ -6,7 +6,7 @@
 /*   By: tbournon <tbournon@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/17 10:00:00 by tbournon          #+#    #+#             */
-/*   Updated: 2022/11/17 10:00:41 by tbournon         ###   ########.fr       */
+/*   Updated: 2022/11/17 10:46:14 by tbournon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,11 @@
 
 void	ft_lstadd_back(t_list **lst, t_list *new)
 {
-	
+	if (lst)
+	{
+		if (*lst)
+			ft_lstlast(*lst)->next = new;
+		else
+			*lst = new;
+	}
 }
