1af5db012444a8f8eded1acc9248239da521fce0
Liquid Platypus <104024934+LiquidPlatypus@users.noreply.github.com>
2022-11-17 14:30:31 +0100

    je sais plus lol

--- Libft/ft_lstmap.c
+++ Libft/ft_lstmap.c
@@ -6,13 +6,33 @@
 /*   By: tbournon <tbournon@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/17 11:58:04 by tbournon          #+#    #+#             */
-/*   Updated: 2022/11/17 12:00:28 by tbournon         ###   ########.fr       */
+/*   Updated: 2022/11/17 13:02:39 by tbournon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-t_list	*ft_lstmap(t_list *lst, void *(*f)(void), void (*del)(void))
+t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
-	
+	t_list	*newlst;
+	t_list	*element;
+	void	*buffer;
+
+	newlst = NULL;
+	if (!lst || !f || !del)
+		return (NULL);
+	while (lst != NULL)
+	{
+		buffer = f(lst->content);
+		element = ft_lstnew(buffer);
+		if (!element)
+		{
+			free(buffer);
+			ft_lstclear(&newlst, del);
+			return (NULL);
+		}
+		ft_lstadd_back(&newlst, element);
+		lst = lst->next;
+	}
+	return (newlst);
 }
