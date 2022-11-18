4cbc2108a8c34ffbffe94ad7e27efaee89c5b26b
Liquid Platypus <104024934+LiquidPlatypus@users.noreply.github.com>
2022-11-18 16:20:05 +0100

    Update ft_printf.c

--- ft_printf/ft_printf.c
+++ ft_printf/ft_printf.c
@@ -6,16 +6,17 @@
 /*   By: tbournon <tbournon@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/18 12:13:00 by tbournon          #+#    #+#             */
-/*   Updated: 2022/11/18 16:09:19 by tbournon         ###   ########.fr       */
+/*   Updated: 2022/11/18 16:19:46 by tbournon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include <stdarg.h>
 
-int	ft_printf(const char *, ...)
+int	ft_printf(const char *input, ...)
 {
 	va_list	params;
 
+	va_start(params, input);
 	return (0);
 }
