a004a4c81d91a26fd267f3eddcbf03565d5f87a5
Liquid Platypus <104024934+LiquidPlatypus@users.noreply.github.com>
2022-11-21 17:08:15 +0100

    Update ft_printf.c

--- ft_printf/ft_printf.c
+++ ft_printf/ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: tbournon <tbournon@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/18 12:13:00 by tbournon          #+#    #+#             */
-/*   Updated: 2022/11/21 16:10:40 by tbournon         ###   ########.fr       */
+/*   Updated: 2022/11/21 16:32:32 by tbournon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,8 +26,6 @@ char	*char_parsing(void *input, char c)
 int	ft_printf(const char *input, ...)
 {
 	va_list		params;
-	int			x;
-	// char		*s;
 
 	if (input == NULL)
 		return (0);
@@ -40,77 +38,11 @@ int	ft_printf(const char *input, ...)
 			input++;
 		}
 		if (*input == 'c' || input == 's')
-			char_parsing(va_arg(param, ), *input);
-	/*	if (*input == 'c')
 		{
-			x = va_arg(params, int);
-			ft_putchar_fd(x, 1);
-			break ;
-		} */
-	/* 	else if (*cur_arg == 's')
-		{
-			s = va_arg(params, char *);
-			// !
-			break ;
-		} */
-	/*	else if (*cur_arg == 'p')
-		{
-			// !
-		} */
-	/* 	else if (*cur_arg == 'd')
-		{
-			x = va_arg(params, int);
-			if (x < 0)
-			{
-				x = -x;
-				ft_putchar_fd('-');
-			}
-			// !
-			break ;
-		} */
-	/*	else if (*cur_arg == 'i')
-		{
-			x = va_arg(params, int);
-			if (x < 0)
-			{
-				x = -x;
-				ft_putchar_fd('-');
-			}
-			// !
-			break ;
-		} */
-	/* else if (*cur_arg == 'u')
-		{
-			// !
-		} */
-	/* 	else if (*cur_arg == 'x')
-		{
-			x = va_arg(params, unsigned int);
-			// !
-			break ;
-		} */
-	/* 	else if (*cur_arg == 'x')
-		{
-			x = va_arg(params, unsigned int);
-			// !
-			break ;
-		} */
-	/* 	else if (*cur_arg == 'o')
-		{
-			x = va_arg(params, unsigned int);
-			// !
-			break ;
-		} */
+			char_parsing(va_arg(param, char *), *input);
+			input++;
+		}
 	}
 	va_end(params);
 	return (0);
-}
-
-
-int main()
-{
-	char c = 'c';
-	ft_printf("bonjour %c\n", c);
-
-	return 0;
 }
