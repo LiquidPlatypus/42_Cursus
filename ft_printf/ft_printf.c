ec7ce6692be1b9c72089c1b4bfcacbc11d4cd5e7
Liquid Platypus <104024934+LiquidPlatypus@users.noreply.github.com>
2022-11-21 11:11:59 +0100

    JE DOIS SÉPARER EN PLUSIEURS FONCTIONS ft_printf

--- ft_printf/ft_printf.c
+++ ft_printf/ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: tbournon <tbournon@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/18 12:13:00 by tbournon          #+#    #+#             */
-/*   Updated: 2022/11/18 16:28:53 by tbournon         ###   ########.fr       */
+/*   Updated: 2022/11/21 11:08:41 by tbournon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,9 +15,26 @@
 
 int	ft_printf(const char *input, ...)
 {
-	va_list	params;
+	va_list		params;
+	int			x;
+	const char	*cur_arg;
 
+	cur_arg = input;
+	if (input == NULL)
+		return (0);
 	va_start(params, input);
+	while (*cur_arg++ != '\0')
+	{
+		while (*cur_arg++ != '%')
+			ft_putchar_fd(*cur_arg, 1);
+		if (*cur_arg == 'c')
+		{
+			x = va_arg(params, int);
+			ft_putchar_fd(x, 1);
+			break ;
+		}
+		else if (*cur)
+	}
 	va_end(params);
 	return (0);
 }
