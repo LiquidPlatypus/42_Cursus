fd4910a810892ede9a750e160fa191d7694ae10a
Liquid Platypus <104024934+LiquidPlatypus@users.noreply.github.com>
2022-11-22 11:43:23 +0100

    ADDED ft_whitespace

--- ft_printf/ft_printf.h
+++ ft_printf/ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: tbournon <tbournon@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/21 14:28:33 by tbournon          #+#    #+#             */
-/*   Updated: 2022/11/22 11:18:40 by tbournon         ###   ########.fr       */
+/*   Updated: 2022/11/22 11:40:25 by tbournon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,5 +19,6 @@
 int		ft_printf(const char *input, ...);
 void	ft_putchar_fd(char c, int fd);
 char	*ft_itoa(int n);
+int	whitespaces(const char str);
 
 #endif
