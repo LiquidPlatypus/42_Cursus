20076d91f8c4a5c9b3aee23c10f2bf4cd1326c72
Liquid Platypus <104024934+LiquidPlatypus@users.noreply.github.com>
2022-11-23 17:42:45 +0100

    Update ft_printstr.c

--- ft_printf/ft_printstr.c
+++ ft_printf/ft_printstr.c
@@ -6,7 +6,7 @@
 /*   By: tbournon <tbournon@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/23 17:05:52 by tbournon          #+#    #+#             */
-/*   Updated: 2022/11/23 17:08:10 by tbournon         ###   ########.fr       */
+/*   Updated: 2022/11/23 17:38:44 by tbournon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,9 @@ int	ft_printstr(char *s)
 
 	len = 0;
 	if (s)
+	{
 		write(1, s, ft_strlen(s));
-	return (len = ft_strlen(s));
+		return (len = ft_strlen(s));
+	}
+	return (0);
 }
