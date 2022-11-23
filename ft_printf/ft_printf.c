7f4be1db271944f2c3373c9fc7495447ec0d81b0
Liquid Platypus <104024934+LiquidPlatypus@users.noreply.github.com>
2022-11-23 17:43:20 +0100

    Update ft_printf.c

--- ft_printf/ft_printf.c
+++ ft_printf/ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: tbournon <tbournon@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/18 12:13:00 by tbournon          #+#    #+#             */
-/*   Updated: 2022/11/23 10:17:06 by tbournon         ###   ########.fr       */
+/*   Updated: 2022/11/23 17:42:35 by tbournon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,9 @@
 
 char	*number_parsing(int input)
 {
-	int	returned_number;
+	char	*returned_number;
 
-	returned_number = 0;
-	return (ft_itoa((int)input));
+	return (returned_number = ft_itoa((int)input));
 }
 
 char	char_parsing(void *input)
@@ -27,35 +26,74 @@ char	char_parsing(void *input)
 	return (returned_char = (char)input);
 }
 
+char	*str_parsing(void *input)
+{
+	char	*returned_str;
+
+	return (returned_str = (char *)input);
+}
+
+/*----------------------------------------------------------------------------------------------------------------------------------*/
+
 int	ft_printf(const char *input, ...)
 {
 	va_list	params;
+	size_t	count;
+	// char	*str;
 
+	count = 0;
 	if (input == NULL)
 		return (0);
+	//  str = 
 	va_start(params, input);
 	while (*input != '\0')
 	{
-		while (*input != '%')
+	/*	while (*input != '%')
 		{
 			if (*input == '\0')
 				return (0);
 			ft_putchar_fd(*input, 1);
 			input++;
-		}
+		} */
 		input++;
-		if (*input == 'c' || *input == 's')
-			ft_putchar_fd(char_parsing(va_arg(params, char *)), 1);
-		if (*input++ == 'd' || *input++ == 'i')
-			ft_putstr_fd(number_parsing(va_arg(params, int)), 1);
+		if (*input == 'c')
+		{	
+			count += ft_printchar(char_parsing(va_arg(params, char *)));
+			input++;
+		}
+		if (*input == 's')
+		{
+			count += ft_printstr(str_parsing(va_arg(params, char *)));
+			input++;
+		}
+	/*	if (*input == 'p')
+		{
+			// !
+			input++;
+		} */
+		if (*input == 'd' || *input == 'i' || *input == 'u')
+		{
+			count += ft_printstr(number_parsing(va_arg(params, int)));
+			input++;
+		}
+		/* if (*input == 'x' || *input == 'X')
+		{
+			
+			input++;
+		} */
 	}
 	va_end(params);
-	return (0);
+	// count = ft_strlen();
+	return (count);
 }
-/*
+
 int	main(void)
 {
-	ft_printf("bonjour %c\n", 'F');
+	int	x;
+
+	x = ft_printf("%cs%cs%c", 'c', 'b', 'a');
+	ft_putnbr_fd(x, 1);
+	ft_printchar('\n');
+	ft_printf("%s", (char *) NULL);
 	return (0);
 }
-*/
