4ba401da79d4d8b0be213b34779daccdf7040701
Liquid Platypus <104024934+LiquidPlatypus@users.noreply.github.com>
2022-11-24 15:11:07 +0100

    Update ft_putstr.c

---
+++ ft_printf/libft/ft_putstr.c
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putstr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tbournon <tbournon@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/24 15:07:16 by tbournon          #+#    #+#             */
+/*   Updated: 2022/11/24 15:10:53 by tbournon         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/ft_printf.h"
+
+int	ft_putstr(char *str, int point)
+{
+	int	count;
+
+	count = 0;
+	while (str[count] && count < point)
+		ft_putchar(str[count++]);
+	return (count);
+}
