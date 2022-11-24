dabdcef03cdebd409ce37069ae971922e9573e4a
Liquid Platypus <104024934+LiquidPlatypus@users.noreply.github.com>
2022-11-24 17:13:03 +0100

    À FINIR ft_printptr

--- ft_printf/sources/ft_printptr.c
+++ ft_printf/sources/ft_printptr.c
@@ -6,27 +6,40 @@
 /*   By: tbournon <tbournon@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/24 16:57:21 by tbournon          #+#    #+#             */
-/*   Updated: 2022/11/24 16:58:38 by tbournon         ###   ########.fr       */
+/*   Updated: 2022/11/24 17:09:21 by tbournon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/ft_printf.h"
 
-static int	ft_minus(char *str, t_print *tab)
+// ! À FINIR -----------
+
+static int	ft_minus(char *ptr, t_print *tab)
 {
 	int	count;
 
 	count = 0;
+	count += ft_putstr("0x", 2);
 	if (tab->point >= 0)
 	{
-		count += ft_len(tab->point, ft_strlen(str), 0);
-		count += ft_putstr(str, tab->point);
+		count += ft_len(tab->point, ft_strlen(ptr), 1);
+		count += ft_putstr(ptr, tab->point);
 	}
 	else
-		count += ft_putstr(str, ft_strlen(str));
+		count += ft_putstr(ptr, ft_strlen(ptr));
+	return (count);
 }
 
 int	ft_printptr(unsigned long long x, t_print *tab)
 {
+	int		count;
+	char	*ptr;
 
+	count = 0;
+	if (x == 0 && tab->point == 0)
+	{
+		count += ft_putstr("0x", 2);
+		return (count += ft_len(tab->total_len, 0, 1));
+	}
+	ptr = 
 }
