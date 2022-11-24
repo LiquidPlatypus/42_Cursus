23ad9df0f970775e126aa5947d1da845853e0bde
Liquid Platypus <104024934+LiquidPlatypus@users.noreply.github.com>
2022-11-24 11:36:18 +0100

    ADDED my struct

--- ft_printf/ft_printf.h
+++ ft_printf/ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: tbournon <tbournon@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/21 14:28:33 by tbournon          #+#    #+#             */
-/*   Updated: 2022/11/24 10:50:37 by tbournon         ###   ########.fr       */
+/*   Updated: 2022/11/24 11:36:08 by tbournon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,21 @@
 
 # include "libft/libft.h"
 # include <stdarg.h>
+
+typedef struct s_print
+{
+	va_list	params;
+	int		width;
+	int		precision;
+	int		zero_padding;
+	int		point;
+	int		dash;
+	int		total_len;
+	int		sign;
+	int		is_zero;
+	int		percent;
+	int		space;
+}	t_print;
 
 int		is_format(int c);
 int		ft_printchar(char c);
