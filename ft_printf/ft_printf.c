158b27586630f864dbef4f75aef05f35f621563f
Liquid Platypus <104024934+LiquidPlatypus@users.noreply.github.com>
2022-11-25 11:36:32 +0100

    JE REPREND À ZÉRO

--- ft_printf/ft_printf.c
+++ ft_printf/ft_printf.c
@@ -6,86 +6,22 @@
 /*   By: tbournon <tbournon@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/18 12:13:00 by tbournon          #+#    #+#             */
-/*   Updated: 2022/11/25 10:16:21 by tbournon         ###   ########.fr       */
+/*   Updated: 2022/11/25 11:21:39 by tbournon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "includes/ft_printf.h"
 
-static t_print	ft_initializer(void)
-{
-	t_print	tab;
-
-	tab.width = 0;
-	tab.precision = 0;
-	tab.zero_padding = 0;
-	tab.point = 0;
-	tab.sign = 0;
-	tab.total_len = 0;
-	tab.is_zero = 0;
-	tab.dash = 0;
-	tab.percent = 0;
-	tab.space = 0;
-	return (tab);
-}
-
-static int	ft_flags_pars(const char *str, int x, t_print tab)
-{
-	while (str[x])
-	{
-		if (!ft_isdigit(str[x]) && !ft_type(str[x]) && !ft_flags(str[x]))
-			break ;
-		if (str[x] == '0' && tab.total_len == 0 && tab.sign == 0)
-			tab.is_zero = 1;
-		if (str[x] == '.')
-			x = ft_point(str, x, tab);
-		if (str[x] == '-')
-			tab = ft_sign(tab);
-		x++;
-	}
-	return (x);
-}
-
-static int	ft_treatment(const char *str, va_list params)
-{
-	int		x;
-	int		count;
-	t_print	tab;
-
-	x = 0;
-	count = 0;
-	while (1)
-	{
-		tab = ft_initializer();
-		if (!str)
-			break ;
-		else if (str[x] == '%' && str[x + 1])
-		{
-			x = ft_flags_pars(str, ++x, tab);
-			if (ft_type(str[x]))
-				count += ft_choose(str[x], tab, params);
-			else if (str[x])
-				count += ft_printchar(str[x], tab);
-		}
-		else if (str[x] != '%')
-			count += ft_printchar(str[x], tab);
-		x++;
-	}
-	return (count);
-}
-
 int	ft_printf(const char *input, ...)
 {
 	va_list		params;
 	size_t		count;
-	const char	*str;
 
-	count = 0;
-	str = ft_strdup(input);
+	if (!input)
+		return (0);
 	va_start(params, input);
-	count = ft_treatment(str, params);
+	
 	va_end(params);
-	free((char *)str);
 	return (count);
 }
 
@@ -93,9 +29,7 @@ int	main(void)
 {
 	int	x;
 
-	x = ft_printf("%cs%cs%c", 'c', 'b', 'a');
-	ft_putnbr_fd(x, 1);
-	ft_putchar('\n');
-	ft_printf("%s", (char *) NULL);
+	x = ft_printf("%c", '0');
+	ft_printf("%c", '0');
 	return (0);
 }
--- ft_printf/includes/ft_printf.h
+++ ft_printf/includes/ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: tbournon <tbournon@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/21 14:28:33 by tbournon          #+#    #+#             */
-/*   Updated: 2022/11/25 10:15:14 by tbournon         ###   ########.fr       */
+/*   Updated: 2022/11/25 11:25:29 by tbournon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,48 +18,17 @@
 
 typedef struct s_print
 {
-	va_list	params;
 	int		width;
-	int		precision;
-	int		zero_padding;
+	int		zero;
 	int		point;
 	int		dash;
-	int		total_len;
-	int		sign;
-	int		is_zero;
+	int		plus;
+	int		minus;
 	int		percent;
 	int		space;
+	char	type;
 }	t_print;
 
-// ft_len.c
 
-int				ft_len(int len, int sign, int is_zero);
-
-// ft_which_format.c
-
-int				ft_type(int c);
-int				ft_flags(int c);
-int				ft_choose(int c, t_print tab, va_list params);
-
-int				ft_isdigit(int c);
-int				ft_printchar(char c, t_print tab);
-int				ft_printf(const char *input, ...);
-int				ft_printint(int x);
-int				ft_printpercent(t_print tab);
-int				ft_printptr(unsigned long long x, t_print tab);
-int				ft_printstr(char *str, t_print tab);
-int				ft_printuint(unsigned int x);
-void			ft_putchar(char c);
-int				ft_putstr(char *str, int point);
-char			*ft_itoa(int n);
-
-// ft_printflags.c
-
-t_print			ft_sign(t_print tab);
-int				ft_point(const char *str, int start, t_print tab);
-
-int				ft_strlen(const char *str);
-
-char			*ft_uitoa(unsigned int n);
 
 #endif
