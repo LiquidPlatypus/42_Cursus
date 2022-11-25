fcb3cc162e1f7101dd6fdbbff226abd9c683c98a
Liquid Platypus <104024934+LiquidPlatypus@users.noreply.github.com>
2022-11-25 13:49:20 +0100

    ADDED ft_params_search

--- ft_printf/sources/ft_params_search.c
+++ ft_printf/sources/ft_params_search.c
@@ -1,29 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*   ft_params_search.c                                 :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: tbournon <tbournon@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/11/21 14:28:33 by tbournon          #+#    #+#             */
-/*   Updated: 2022/11/25 13:49:03 by tbournon         ###   ########.fr       */
+/*   Created: 2022/11/25 13:47:38 by tbournon          #+#    #+#             */
+/*   Updated: 2022/11/25 13:48:46 by tbournon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef FT_PRINTF_H
-# define FT_PRINTF_H
+#include "../includes/ft_printf.h"
 
-# include "../libft/libft.h"
-# include <stdarg.h>
-
-typedef struct s_print
+const char	*ft_params_search(va_list params, const char *input, t_print *tab)
 {
-	int	width;
-	int	total_len;
-}	t_print;
 
-// ft_params_search.c
-
-const char	*ft_params_search(va_list params, const char *input, t_print *tab);
-
-#endif
+}
