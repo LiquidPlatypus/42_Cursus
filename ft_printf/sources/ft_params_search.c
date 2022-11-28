d24e0eb1dbf0b683cd840da0ca768834615ae173
Liquid Platypus <104024934+LiquidPlatypus@users.noreply.github.com>
2022-11-28 10:47:08 +0100

    CHANGED ft_params_search prototype

--- ft_printf/sources/ft_params_search.c
+++ ft_printf/sources/ft_params_search.c
@@ -6,13 +6,13 @@
 /*   By: tbournon <tbournon@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/25 13:47:38 by tbournon          #+#    #+#             */
-/*   Updated: 2022/11/25 14:06:32 by tbournon         ###   ########.fr       */
+/*   Updated: 2022/11/28 10:46:03 by tbournon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/ft_printf.h"
 
-const char	*ft_params_search(va_list params, const char *input)
+const char	*ft_params_search(va_list params, const char *input, t_print tab)
 {
 	int	count;
 
