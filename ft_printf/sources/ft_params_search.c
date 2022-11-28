8751c6358830f3d727bddab13d514e1e6bfb8717
Liquid Platypus <104024934+LiquidPlatypus@users.noreply.github.com>
2022-11-28 18:52:57 +0100

    ADDED ft_hexa

--- ft_printf/sources/ft_params_search.c
+++ ft_printf/sources/ft_params_search.c
@@ -6,7 +6,7 @@
 /*   By: tbournon <tbournon@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/25 13:47:38 by tbournon          #+#    #+#             */
-/*   Updated: 2022/11/28 13:27:11 by tbournon         ###   ########.fr       */
+/*   Updated: 2022/11/28 18:52:43 by tbournon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,13 +19,13 @@ int	ft_params_search(va_list params, const char *input)
 	if (*input == 's')
 		return (ft_string(va_arg(params, char *)));
 /*	if (*input == 'p')
-		return (ft_pointer(va_arg(params, void *), tab)); */
+		return (ft_pointer(va_arg(params, void *))); */
 	if (*input == 'd' || *input == 'i')
 		return (ft_int(va_arg(params, int)));
 	if (*input == 'u')
 		return (ft_uint(va_arg(params, unsigned int)));
-/*	if (*input == 'x' || *input == 'X')
-		return (ft_hexa(va_arg(params, int), tab)); */
+	if (*input == 'x' || *input == 'X')
+		return (ft_hexa(va_arg(params, int)));
 	if (*input == '%')
 		return (ft_percent());
 	return (-1);
