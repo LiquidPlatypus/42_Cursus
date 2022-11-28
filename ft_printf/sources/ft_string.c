87d358eed87ecebd9f2e29e1ac714b8f14e20a63
Liquid Platypus <104024934+LiquidPlatypus@users.noreply.github.com>
2022-11-28 10:26:17 +0100

    ADDED NULL condition

--- ft_printf/sources/ft_string.c
+++ ft_printf/sources/ft_string.c
@@ -6,7 +6,7 @@
 /*   By: tbournon <tbournon@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/25 23:38:14 by tbournon          #+#    #+#             */
-/*   Updated: 2022/11/25 23:38:14 by tbournon         ###   ########.fr       */
+/*   Updated: 2022/11/28 10:25:20 by tbournon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,12 @@ int	ft_string(char *input)
 {
 	int	count;
 
-	count = ft_putstr(input);
+	if (!input)
+	{
+		ft_putstr("(null");
+		count = 0;
+	}
+	else
+		count = ft_putstr(input);
 	return (count);
 }
