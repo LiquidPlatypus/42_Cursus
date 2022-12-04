1f2c01ec0d1e7aabb3f8ba350c06676a9d9a7354
Liquid Platypus <104024934+LiquidPlatypus@users.noreply.github.com>
2022-12-04 13:41:51 +0100

    ADDED some comments explaining the functions

--- get_next_line/get_next_line_utils.c
+++ get_next_line/get_next_line_utils.c
@@ -12,20 +12,31 @@
 
 #include "get_next_line.h"
 
+// Uses read() to add characters to the stash
+
 void	read_and_stash()
 {
 
 }
+
+// Adds the content of our buffer to the end of our stash
 
 void	add_to_stash()
 {
 
 }
 
+/* Extracts all characters from our stash and stores them in out line.
+   Stoping after the first \n it encouters */
+
 void	extract_line()
 {
 
 }
+
+/* After extracting the line that was read, we don't need those characters anymore.
+   This function clears the stash so only the characters that have not been returned at
+   the end of get_next_line() remain in our static stash. */
 
 void	clean_stash()
 {
