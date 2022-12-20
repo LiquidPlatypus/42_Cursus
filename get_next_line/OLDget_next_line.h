35c1d2834ea77fc8d3e17ff8c1d5d8b57b800a6d
Liquid Platypus <104024934+LiquidPlatypus@users.noreply.github.com>
2022-12-20 11:24:52 +0100

    je recommence encore une fois

--- get_next_line/OLDget_next_line.h
+++ get_next_line/OLDget_next_line.h
@@ -5,8 +5,31 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: tbournon <tbournon@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/12/20 11:24:28 by tbournon          #+#    #+#             */
-/*   Updated: 2022/12/20 11:24:29 by tbournon         ###   ########.fr       */
+/*   Created: 2022/12/04 11:08:09 by tbournon          #+#    #+#             */
+/*   Updated: 2022/12/19 13:24:22 by tbournon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <stdlib.h>
+# include <unistd.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 1
+# endif
+
+// get_next_line.c
+
+char	*get_next_line(int fd);
+
+// get_next_line_utils.c
+
+int		ft_strlen(const char *str);
+char	*ft_strchr(const char *s, int c);
+char	*ft_strjoin(char *s1, char *s2);
+char	*ft_get_line(char *left_str);
+char	*ft_new_left_str(char *left_str);
+
+#endif
--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -5,67 +5,8 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: tbournon <tbournon@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/12/04 11:04:40 by tbournon          #+#    #+#             */
-/*   Updated: 2022/12/19 17:08:11 by tbournon         ###   ########.fr       */
+/*   Created: 2022/12/20 11:24:21 by tbournon          #+#    #+#             */
+/*   Updated: 2022/12/20 11:24:30 by tbournon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "get_next_line.h"
-#include <fcntl.h> // !                                 
-#include <stdio.h> // !                                 
-
-char	*ft_read_to_left_str(int fd, char *left_str)
-{
-	char	*buffer;
-	int		byt_readed;
-
-	buffer = malloc((BUFFER_SIZE + 1) * sizeof(char));
-	if (!buffer)
-		return (NULL);
-	byt_readed = 1;
-	while (!ft_strchr(left_str, '\n') && byt_readed != 0) // * si strjoin trop long, check si byt_readed = 0
-	{
-		byt_readed = read(fd, buffer, BUFFER_SIZE);
-		if (byt_readed == -1)
-		{
-			free(buffer);
-			return (NULL);
-		}
-		buffer[byt_readed] = '\0';
-		left_str = ft_strjoin(left_str, buffer);
-	}
-	free(buffer);
-	return (left_str);
-}
-
-char	*get_next_line(int fd)
-{
-	char		*line;
-	static char	*left_str;
-
-	if (fd < 0 || BUFFER_SIZE <= 0)
-		return (0);
-	left_str = ft_read_to_left_str(fd, left_str);
-	if (!left_str)
-		return (NULL);
-	// ! d'abord check si haine vide, si pas vide, doit la free et appeler une deuxième fois pour return NULL
-	line = ft_get_line(left_str);
-	left_str = ft_new_left_str(left_str);
-	return (line);
-}
-
-
-int main()
-{
-	int x;
-	char *str;
-
-	x = open("variable_nls.txt", O_RDONLY);
-
-	while ((str = get_next_line(x)))
-		printf("%s", str);
-	puts(".");
-	printf("%s", str);
-
-	return (0);
-}
