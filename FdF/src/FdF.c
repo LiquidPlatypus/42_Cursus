da8318e4ee6ffbd2b5bb2a608f3e23f8a9e11ce0
Liquid Platypus <104024934+LiquidPlatypus@users.noreply.github.com>
2022-12-27 11:12:28 +0100

    Update FdF.c

--- FdF/src/FdF.c
+++ FdF/src/FdF.c
@@ -6,7 +6,7 @@
 /*   By: tbournon <tbournon@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/27 10:00:15 by tbournon          #+#    #+#             */
-/*   Updated: 2022/12/27 11:10:13 by tbournon         ###   ########.fr       */
+/*   Updated: 2022/12/27 11:12:01 by tbournon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,29 +15,29 @@
 #include <stdio.h> // !                                                 
 #include <unistd.h> // !                                                
 
-void	my_keyhook(mlx_key_data_t keydata, void *param)
+void my_scrollhook(double xdelta, double ydelta, void* param)
 {
-	// If we PRESS the 'J' key, printf "Hello".
-	if (keydata.key == MLX_KEY_J && keydata.action == MLX_PRESS)
-		puts("Hello ");
+	// Simple up or down detection.
+	if (ydelta > 0)
+		puts("Up!");
+	else if (ydelta < 0)
+		puts("Down!");
 
-	// If we RELEASE the 'K' key, print "World".
-	if (keydata.key == MLX_KEY_K && keydata.action == MLX_RELEASE)
-		puts("Wolrd");
-
-	// If we HOLD the 'L' key, print "!".
-	if (keydata.key == MLX_KEY_L && keydata.action == MLX_REPEAT)
-		puts("!");
+	// Can also detect a mousewheel that go along the X (e.g: MX Master 3)
+	if (xdelta < 0)
+		puts("Sliiiide to the left!");
+	else if (xdelta > 0)
+		puts("Sliiiide to the right!");
 }
 
-int	main(void)
+int32_t	main(void)
 {
-	mlx_t	*mlx;
+	mlx_t* mlx;
 
-	if (!(mlx = mlx_init(WIDTH, HEIGTH, "MLX42", true)))
+	if (!(mlx = mlx_init(WIDTH, HEIGHT, "MLX42", true)))
 		return (EXIT_FAILURE);
 
-	mlx_key_hook(mlx, &my_keyhook, NULL);
+	mlx_scroll_hook(mlx, &my_scrollhook, NULL);
 	mlx_loop(mlx);
 	mlx_terminate(mlx);
 	return (EXIT_SUCCESS);
