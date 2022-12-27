5292bf1d787c485eded71a3ada3b2358070440d4
Liquid Platypus <104024934+LiquidPlatypus@users.noreply.github.com>
2022-12-27 10:56:34 +0100

    Affiche un carré blanc

--- FdF/src/FdF.c
+++ FdF/src/FdF.c
@@ -6,7 +6,7 @@
 /*   By: tbournon <tbournon@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/27 10:00:15 by tbournon          #+#    #+#             */
-/*   Updated: 2022/12/27 10:50:13 by tbournon         ###   ########.fr       */
+/*   Updated: 2022/12/27 10:56:24 by tbournon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,8 @@
 #include <stdlib.h> // !                                                
 #include <stdio.h> // !                                                 
 #include <unistd.h> // !                                                
+#include <string.h> // !                                                
+#define BPP sizeof(int32_t) // !                                        
 
 static void	ft_error(void)
 {
@@ -38,12 +40,13 @@ int	main(void)
 	/* Do stuff */
 
 	// Create and display the image.
-	mlx_image_t	*img = mlx_new_image(mlx, 256, 256);
+	mlx_image_t	*img = mlx_new_image(mlx, 128, 128);
+	
+	// Set the channels of each pixel in our image to the maximum byte value of 255
+	memset(img->pixels, 255, img->width * img->height * BPP);
+
 	if (!img || (mlx_image_to_window(mlx, img, 0, 0) < 0))
 		ft_error();
-
-	// Even after the image is being displayed, we can still modifi the buffer.*
-	mlx_put_pixel(img, 128, 128, 0xFF0000FF);
 
 	// Register a hook and âss mlx as an optional param.
 	// NOTE: Do this before calling mlx_loop!
