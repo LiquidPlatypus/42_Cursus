fbdec39bdfa1dfcc2ec22176839d84b73a39531b
Liquid Platypus <104024934+LiquidPlatypus@users.noreply.github.com>
2023-01-04 09:42:16 +0100

    J'ai réussi à tracer une ligne

--- FdF/src/FdF.c
+++ FdF/src/FdF.c
@@ -6,7 +6,7 @@
 /*   By: tbournon <tbournon@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/27 10:00:15 by tbournon          #+#    #+#             */
-/*   Updated: 2022/12/27 12:14:01 by tbournon         ###   ########.fr       */
+/*   Updated: 2023/01/04 09:41:40 by tbournon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,29 +15,50 @@
 #include <stdio.h> // !                                                 
 #include <unistd.h> // !                                                
 
-void my_scrollhook(double xdelta, double ydelta, void* param)
+static void	ft_hook(void *param)
 {
-	// Simple up or down detection.
-	if (ydelta > 0)
-		puts("Up!");
-	else if (ydelta < 0)
-		puts("Down!");
+	const mlx_t	*mlx;
 
-	// Can also detect a mousewheel that go along the X (e.g: MX Master 3)
-	if (xdelta < 0)
-		puts("Sliiiide to the left!");
-	else if (xdelta > 0)
-		puts("Sliiiide to the right!");
+	mlx = param;
+	// TODO : A REMPLACER AVEC MON PROPRE PRINTF
+	printf("WIDTH: %d | HEIGHT: %d\n", mlx->width, mlx->height);
 }
 
-int32_t	main(void)
+static void	line(mlx_image_t *img)
 {
-	mlx_t* mlx;
+	int		y1;
+	int		x;
+	float	y;
+	int		width;
+	int		height;
 
-	if (!(mlx = mlx_init(WIDTH, HEIGHT, "MLX42", true)))
+	y1 = 0;
+	x = 0;
+	y = 0;
+	width = WIDTH;
+	height = 0;
+	while (x < width)
+	{
+		y = 0.7 * x + y1;
+		mlx_put_pixel(img, x, y, 0xFF000FF);
+		x++;
+	}
+}
+
+int	main(void)
+{
+	mlx_t		*mlx;
+	mlx_image_t	*img;
+
+	mlx = mlx_init(WIDTH, HEIGHT, "FdF", true);
+	if (!mlx)
 		return (EXIT_FAILURE);
-
-	mlx_scroll_hook(mlx, &my_scrollhook, NULL);
+	img = mlx_new_image(mlx, WIDTH, HEIGHT);
+	if (!img || (mlx_image_to_window(mlx, img, 0, 0) < 0))
+		return (EXIT_FAILURE);
+	//mlx_put_pixel(img, 128, 128, 0xFF000FF);
+	line(img);
+	mlx_loop_hook(mlx, ft_hook, mlx);
 	mlx_loop(mlx);
 	mlx_terminate(mlx);
 	return (EXIT_SUCCESS);
