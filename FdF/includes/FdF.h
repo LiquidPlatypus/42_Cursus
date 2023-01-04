8e54fac45008c2ca6b0ee4f4818fcdc30cff787c
Liquid Platypus <104024934+LiquidPlatypus@users.noreply.github.com>
2023-01-04 11:13:59 +0100

    struct pour ma ligne

--- FdF/includes/FdF.h
+++ FdF/includes/FdF.h
@@ -6,7 +6,7 @@
 /*   By: tbournon <tbournon@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/27 10:00:23 by tbournon          #+#    #+#             */
-/*   Updated: 2022/12/27 11:11:40 by tbournon         ###   ########.fr       */
+/*   Updated: 2023/01/04 10:53:12 by tbournon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,17 @@
 
 # include "../MLX42/include/MLX42/MLX42.h"
 
-# define WIDTH 256
-# define HEIGHT 256
+typedef struct line_params
+{
+	int		x1;
+	int		y1;
+	int		x2;
+	int		y2;
+	int		x;
+	float	y;
+}		t_line;
+
+# define WIDTH 1200
+# define HEIGHT 1000
 
 #endif
