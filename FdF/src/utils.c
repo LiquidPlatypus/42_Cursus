372e0b9732cb3be262a4b1fd43ae3b9d329f4c99
Liquid Platypus <104024934+LiquidPlatypus@users.noreply.github.com>
2023-01-10 11:11:59 +0100

    Update utils.c

--- FdF/src/utils.c
+++ FdF/src/utils.c
@@ -6,8 +6,13 @@
 /*   By: tbournon <tbournon@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/10 11:10:54 by tbournon          #+#    #+#             */
-/*   Updated: 2023/01/10 11:11:07 by tbournon         ###   ########.fr       */
+/*   Updated: 2023/01/10 11:11:52 by tbournon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/FdF.h"
+
+void	initializer(t_line line)
+{
+
+}
